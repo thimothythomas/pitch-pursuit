cb316acd618f01904521396640c0f47645de6f4f
Timothy Thomas Joseph <137427464+thimothythomas@users.noreply.github.com>
2025-02-24 11:51:22 +0530

    Add files via upload

---
+++ ngcc.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+    packages: {
+      'angular2_photoswipe': {
+        ignorableDeepImportMatchers: [
+          /photoswipe\//,
+        ]
+      },
+      '@ckeditor/ckeditor5-angular': {
+        ignorableDeepImportMatchers: [
+          /@ckeditor\//,
+        ]
+      },
+      'ng-apexcharts': {
+        ignorableDeepImportMatchers: [
+          /apexcharts\//,
+        ]
+      },
+      'ng2-ace-editor': {
+        ignorableDeepImportMatchers: [
+          /brace\//,
+        ]
+      },
+      'ng5-slider': {
+        ignorableDeepImportMatchers: [
+          /ng5-slider\//,
+        ]
+      },
+      'angular-vertical-timeline': {
+        ignorableDeepImportMatchers: [
+          /angular-vertical-timeline\//,
+        ]
+      },
+      'angular-typing-animation': {
+        ignorableDeepImportMatchers: [
+          /rxjs\//,
+        ]
+      },
+      'ng2-scroll-to-el': {
+        ignorableDeepImportMatchers: [
+          /rxjs\//,
+        ]
+      },
+      'ngx-swiper-wrapper': {
+        ignorableDeepImportMatchers: [
+          /swiper\//,
+        ]
+      },
+    },
+  };
